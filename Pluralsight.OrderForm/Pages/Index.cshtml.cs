19c094c88ec2314c3f9c120205ac9b68279d0c79
Andy Butland <abutland73@gmail.com>
2026-02-14 12:10:55 +0100

    Add starting state for lab.

--- Pluralsight.OrderForm/Pages/Index.cshtml.cs
+++ Pluralsight.OrderForm/Pages/Index.cshtml.cs
@@ -6,27 +6,11 @@ namespace Pluralsight.OrderForm.Pages;
 
 public class IndexModel : PageModel
 {
-    [BindProperty]
-    public Order Order { get; set; } = new();
-
     public void OnGet()
     {
     }
 
-    public IActionResult OnPost()
+    public void OnPost()
     {
-        if (Order.Product == "Mug")
-        {
-            ModelState.AddModelError("Order.Product", "Sorry, all mugs have been given away.");
-        }
-
-        if (!ModelState.IsValid)
-        {
-            return Page();
-        }
-
-        // Process the order here (e.g., save to database).
-
-        return RedirectToPage("OrderConfirmation");
     }
 }
