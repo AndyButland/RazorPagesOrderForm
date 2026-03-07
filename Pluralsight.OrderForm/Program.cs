4428df8c24c05dd314d4a61a7a0bcc77f8fb25cb
Andy Butland <abutland73@gmail.com>
2026-03-07 11:58:15 +0100

    Add lab specific setup.

--- Pluralsight.OrderForm/Program.cs
+++ Pluralsight.OrderForm/Program.cs
@@ -1,8 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Lab environment specific: this stops .NET from automatically blocking iframes.
+builder.Services.AddAntiforgery(options =>
+{
+    options.SuppressXFrameOptionsHeader = true;
+});
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Lab environment specific: set the security headers.
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("Content-Security-Policy", "frame-ancestors 'self' https://app.pluralsight.com");
+
+    // Remove the restrictive legacy header if something else injected it
+    context.Response.Headers.Remove("X-Frame-Options");
+
+    await next();
+});
 
 app.UseRouting();
 
